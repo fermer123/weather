52885f09a9accadd82d3abaca8f25b25c1baf2f3
fermer123 <fermer_525@mail.ru>
2022-06-04 20:55:38 +0500

    123

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -5,12 +5,15 @@ import style from './Page.module.scss';
 const Page = () => {
   const [weather, setWeather] = useState({});
   const [location, setLocation] = useState('');
-  const api = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=3fc69a436f78882d60a5e06ac53b6cba`;
+
+  const api = `http://api.weatherapi.com/v1/current.json?key=6f82136fec71416b8f6113446220406&&q=${location}&aqi=no`;
+
   const searchLocation = (e) => {
     if (e.key === 'Enter') {
       axios.get(api).then((response) => {
         setWeather(response.data);
       });
+
       setLocation('');
     }
   };
@@ -31,39 +34,37 @@ const Page = () => {
       <div className={style.container}>
         <div className={style.top}>
           <div className={style.location}>
-            {weather ? <p>{weather.name}</p> : null}
+            {weather.location ? <p>{weather.location.name}</p> : null}
           </div>
           <div className={style.temp}>
-            {weather.main ? (
-              <h1>{Math.ceil(weather.main.temp / 32)} °C</h1>
+            {weather.current ? (
+              <h1>{Math.ceil(weather.current.temp_c)} °C</h1>
             ) : null}
           </div>
           <div className={style.description}>
-            {weather.description ? (
-              <p>{weather.weather[0].description}</p>
-            ) : null}
+            {weather.current ? <p>{weather.current.condition.text}</p> : null}
           </div>
         </div>
         <div className={style.bottom}>
           <div className={style.feels}>
-            {weather.main ? (
+            {weather.current ? (
               <p className={style.bold}>
-                {Math.ceil(weather.main.feels_like / 32)} °C
+                {Math.ceil(weather.current.feelslike_c)} °C
               </p>
             ) : null}
 
             <p>Feels like</p>
           </div>
           <div className={style.humidity}>
-            {weather.main ? (
-              <p className={style.bold}>{weather.main.humidity} %</p>
+            {weather.current ? (
+              <p className={style.bold}>{weather.current.humidity} %</p>
             ) : null}
 
             <p>Humidity</p>
           </div>
           <div className={style.wind}>
-            {weather.wind ? (
-              <p className={style.bold}>{weather.wind.speed} m/s</p>
+            {weather.current ? (
+              <p className={style.bold}>{weather.current.wind_kph} kph</p>
             ) : null}
             <p>Wind speed</p>
           </div>
